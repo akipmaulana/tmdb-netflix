aa5752042961d39eb88bf0401c13506990d7c085
Akip Maulana <akip.maulana@gmail.com>
2021-06-26 13:05:29 +0700

    refresh gitignore

---
+++ app/tmdb-netflix/tmdb-netflix/App/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  tmdb-netflix
+//
+//  Created by Akirah Dev on 26/06/21.
+//  
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    internal var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        configureWindow()
+        return true
+    }
+
+    // MARK: - Private Setup
+    private func configureWindow() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+    }
+}
